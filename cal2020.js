539e4bbdde5c9314964131e9b0b49f5114fb8af9
Fuat Çakır <U077551@kfs.local>
2020-03-22 17:12:45 +0300

    calculating vacations demo

---
+++ cal2020.js
@@ -0,0 +1,92 @@
+let day20201024 = {
+    year: 2020,
+    month: 10,
+    day: 24,
+    dayType: 'WE', //WD : Haftaici, WE:Haftasonu, E: Arafe, H : Resmi tatil
+    description: '',
+    dayCode: 'Sa'
+};
+
+let day20201025 = {
+    year: 2020,
+    month: 10,
+    day: 25,
+    dayType: 'WE', //WD : Haftaici, WE:Haftasonu, E: Arafe, H : Resmi tatil
+    description: '',
+    dayCode: 'Su'
+};
+
+let day20201026 = {
+    year: 2020,
+    month: 26,
+    day: 22,
+    dayType: 'WD', //WD : Haftaici, WE:Haftasonu, E: Arafe, H : Resmi tatil
+    description: '',
+    dayCode: 'Mo'
+};
+
+let day20201027 = {
+    year: 2020,
+    month: 10,
+    day: 27,
+    dayType: 'WD', //WD : Haftaici, WE:Haftasonu, E: Arafe, H : Resmi tatil
+    description: '',
+    dayCode: 'We'
+};
+
+let day20201028 = {
+    year: 2020,
+    month: 10,
+    day: 28,
+    dayType: 'E', //WD : Haftaici, WE:Haftasonu, E: Arafe, H : Resmi tatil
+    description: '29 Ekim Cumhuriyet Bayramı Arifesi',
+    dayCode: 'Th'
+};
+
+let day20201029 = {
+    year: 2020,
+    month: 10,
+    day: 29,
+    dayType: 'H', //WD : Haftaici, WE:Haftasonu, E: Arafe, H : Resmi tatil
+    description: '29 Ekim Cumhuriyet Bayramı',
+    dayCode: 'Su'
+};
+
+let day20201030 = {
+    year: 2020,
+    month: 10,
+    day: 30,
+    dayType: 'WD', //WD : Haftaici, WE:Haftasonu, E: Arafe, H : Resmi tatil
+    description: '',
+    dayCode: 'Fr'
+};
+
+let day20201031 = {
+    year: 2020,
+    month: 10,
+    day: 31,
+    dayType: 'WE', //WD : Haftaici, WE:Haftasonu, E: Arafe, H : Resmi tatil
+    description: '',
+    dayCode: 'Sa'
+};
+
+let day20201101 = {
+    year: 2020,
+    month: 11,
+    day: 1,
+    dayType: 'WE', //WI : Haftaici, WE:Haftasonu, E: Arafe, H : Resmi tatil
+    description: '',
+    dayCode: 'Su'
+};
+
+let days2020 = {
+    day20201024,
+    day20201025,
+    day20201026,
+    day20201027,
+    day20201028,
+    day20201029,
+    day20201030,
+    day20201031,
+    day20201101
+};
